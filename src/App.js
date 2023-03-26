4bd2928efbcd529c66bedb71984dd55191d39028
kaminogema <kseniiakopetska@gmail.com>
2023-03-26 21:11:18 +0200

    added basic styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import './App.css';
+import Weather from './Weather';
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <h1>Weather Application</h1>
+        <Weather />
       <footer>
         This project was coded by <a href="https://portfolio-kopetska.netlify.app/" target="_blank">Kseniia Kopetska</a> and is {" "} <a href="https://github.com/kaminogema/weather-app-react-final" target="_blank">
 open-sourced on GitHub
