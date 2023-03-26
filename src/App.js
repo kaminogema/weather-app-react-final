bfdced0ea7e5ac3d7e831c4f2f2e88caf3c31d69
kaminogema <kseniiakopetska@gmail.com>
2023-03-26 16:14:01 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather Application
+      <div className="container">
+      <h1>Weather Application</h1>
       <footer>
         This project was coded by <a href="https://portfolio-kopetska.netlify.app/" target="_blank">Kseniia Kopetska</a> and is {" "} <a href="https://github.com/kaminogema/weather-app-react-final" target="_blank">
 open-sourced on GitHub
       </a>
-      </footer>     
+      </footer>   
+      </div>         
     </div>
   );
 }
