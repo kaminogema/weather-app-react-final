f3f6ee4166c1526948f499bca9976feb84f6ae21
kaminogema <kseniiakopetska@gmail.com>
2023-03-26 16:00:13 +0200

    added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-     Weather Application
+      Weather Application
+      <footer>
+        This project was coded by <a href="https://portfolio-kopetska.netlify.app/" target="_blank">Kseniia Kopetska</a> and is {" "} <a href="https://github.com/kaminogema/weather-app-react-final" target="_blank">
+open-sourced on GitHub
+      </a>
+      </footer>     
     </div>
   );
 }
